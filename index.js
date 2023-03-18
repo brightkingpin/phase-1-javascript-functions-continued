2dcb5619b6b26e64ffaceee03947dfba856bd1cf
brightkingpin <luvandabright781@gmail.com>
2023-03-18 17:23:09 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// Define saturdayFun function with default parameter
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+  }
+
+
+// Define mondayWork function as function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+// This is a function that returns another function which wraps an adjective in a given emphasis and returns a string with a personalized message.
+function wrapAdjective(emphasis = "*") {
+    return function(adjective = "special") {
+      return `You are ${emphasis}${adjective}${emphasis}!`;
+    };
+  }
+  
